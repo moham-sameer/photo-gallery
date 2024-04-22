7314f21a9d512d04c06d65c064251fe13f0f3942
Mohammad Sameer <108628386+moham-sameer@users.noreply.github.com>
2024-04-22 15:51:44 +0530

    Update index.js

--- servers/index.js
+++ servers/index.js
@@ -4,7 +4,13 @@ const cors = require('cors')
 require('./db/connect.js')
 const userRouter = require('./routes/route.js')
 app.use(express.json())
-app.use(cors());
+app.use(cors(
+   {
+  origin:'*',
+  methods: ['POST', 'GET'],
+  credentials: true
+}
+));
 app.use('/',userRouter)
 
 
