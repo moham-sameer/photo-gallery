471c8b54d389361a512bf4b542f99af34517baf2
Mohammad Sameer <108628386+moham-sameer@users.noreply.github.com>
2024-04-22 15:34:08 +0530

    Update index.js

--- servers/index.js
+++ servers/index.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 require('./db/connect.js')
 const userRouter = require('./routes/route.js')
 app.use(express.json())
-app.use(cors({
+app.use(cors(
+  {
   origin:["https://mern-form-client.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
-}))
+}
+));
 app.use('/',userRouter)
 
 
